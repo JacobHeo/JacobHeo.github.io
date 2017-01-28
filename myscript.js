cc74899e112777bf5ab2107943d1b9175a293eae
heo02162001 <heo02162001@gmail.com>
2017-01-28 13:09:28 -0500

    add color

--- myscript.js
+++ myscript.js
@@ -1,3 +1,11 @@
 function changeColor (){
   document.getElementById("content").style.color = "green";
 }
+var operand1;
+var operand2;
+
+function getNumbers(){
+  operand1 = Math.floor(Math.random() * 20) + 1;
+  operand2 = Math.floor(Math.random() * 20) + 1;
+  documetn.getElementById("operadn1").
+}
