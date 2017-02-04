a9eb0d5c5fb8fee6b6309a50d989ede94e898d1e
heo02162001 <heo02162001@gmail.com>
2017-02-04 12:55:40 -0500

    add gallery html

--- myscript.js
+++ myscript.js
@@ -24,3 +24,28 @@ function checkAnswer(){
     document.getElementById("results").innerHTML="Incorrect!";
   }
 }
+
+//this is a list of urls of photos
+var pics = [
+           "http://www.flags.net/images/largeflags/SKOR0001.GIF",
+           "https://sc-news.s3.amazonaws.com/18988/KGS+violin2.jpg",
+           "https://signup.leagueoflegends.com/theme/signup_theme/img/signup_logo2_clean.png"
+           ];
+
+var pickIndex = 0;
+
+function goLeft(){
+  
+  if(picIndex > 0 ){
+    picIndex = picIndex - 1;
+    document.getElementById("pic").src=pics[picIndex];
+  }
+  
+}//end of function
+
+function goRight(){
+  if(picIndex < (pics.length - 1)){
+    picIndex = picInde + 1;
+    document.getElementById("pic").src=pics[picIndex];
+  }
+}
